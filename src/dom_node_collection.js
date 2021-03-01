1a67394920b661498395ac3124749f6edbb53e0d
Christian <cwooddel@u.rochester.edu>
2021-03-01 14:09:23 -0500

    continue project

--- src/dom_node_collection.js
+++ src/dom_node_collection.js
@@ -19,8 +19,47 @@ export class domNodeCollection {
         })
     }
 
-    
+    append(arg) {
+        arg.forEach (ele => {
+            this.htmlEls.innerHTML.append(ele.outerHTML)
+        })
+    }
 
+    attr() {
+        let attrs = {};
+        this.htmlEls.forEach ( ele => {
+            attrs[ele] = ele.innerHTML
+        })
+        return attrs;
+    }
+    
+    addClass(newClass) {
+        this.htmlEls.forEach(ele => {
+            ele.classlist.add(newClass)
+        });
+    }
+
+    removeClass(newClass) {
+        this.htmlEls.forEach(ele => {
+            ele.classlist.remove(newClass)
+        });
+    }
+
+    children() {
+        let childrens = new domNodeCollection();
+        this.htmlEls.forEach((ele) => {
+            childrens.htmlEls.append(ele.children)
+        });
+        return childrens;
+    }
+
+    parent() {
+        let parents = new domNodeCollection();
+        this.htmlEls.forEach((ele) => {
+            parents.htmlEls.append(ele.parentNode)
+        });
+        return parents;
+    }
 }
 
 
